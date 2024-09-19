35a5dd56c77af71b6f9c1aab1a3a8b600e7cd805
Ramazan <ramazanakdil55@gmail.com>
2024-09-19 21:45:12 +0300

    ..

---
+++ CvProje1/Controllers/SkillController.cs
@@ -0,0 +1,52 @@
+﻿using CvProje1.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProje1.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
+    }
+}
